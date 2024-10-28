dfe715ba998901e90e719077605366c9a7dd3d5c
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2024-10-28 13:04:00 +0000

    Aula de javascript

---
+++ AulasJS-backend/introdução/atribuição.js
@@ -0,0 +1,27 @@
+// assignment operator
+let a = 7;
+console.log("Assignment: a = 7, a =", a);
+
+// addition assignment operator
+a += 5;  // a = a + 5
+console.log("Addition Assignment: a += 5, a =", a);
+
+// subtraction assignment operator
+a -= 5;  // a = a - 5
+console.log("Subtraction Assignment: a -= 5, a =", a);
+
+// multiplication assignment operator
+a *= 2;  // a = a * 2
+console.log("Multiplication Assignment: a *= 2, a =", a);
+
+// division assignment operator
+a /= 2;  // a = a / 2
+console.log("Division Assignment: a /= 2, a =", a);
+
+// remainder assignment operator
+a %= 2;  // a = a % 2
+console.log("Remainder Assignment: a %= 2, a =", a);
+
+// exponentiation assignment operator
+a **= 2;  // a = a**2
+console.log("Exponentiation Assignment: a **= 7, a =", a);
