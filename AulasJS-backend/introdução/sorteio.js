6c210836463c34d5f5508b036fc1f4c60e5961c9
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2024-11-11 12:47:46 +0000

    Aula JS

---
+++ AulasJS-backend/introdução/sorteio.js
@@ -0,0 +1,13 @@
+console.log(parseInt(Math.random() * 60 + 1));
+
+for(let i = 0; i < 6; i++){
+    console.log()
+
+}
+
+// guardar variavies array 
+
+// comparar com o primeiro numero 
+
+// dentro do if fazer um while 
+
---
+++ AulasJS-backend/introdução/tabuada.js
@@ -0,0 +1,9 @@
+// Loop para a tabuada
+for ( let j = 1;  j <= 10; j++){
+    console.log(`.:: Tabuada de ${j} ::. \n`);
+
+   for (let i = 1; i <=10; i++){
+    console.log(`\t ${j} X ${i} = ${i * j}`)
+}
+
+}
