6d1f2a78cb3f0b862496b1aba7cd22d4d1eec1b7
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2024-11-12 23:46:28 +0000

    Continuação do código do sorteio

--- AulasJS-backend/introdução/sorteio.js
+++ AulasJS-backend/introdução/sorteio.js
@@ -1,15 +1,24 @@
-function getRandomInt(max){
+ function getRandomInt(max){
    return Math.floor(Math.random() * max);
 
+ }
+
+ let sorteio = [];
+ for(let i = 0 ; i < 6 ; i++){
+    let numero = getRandomInt(60) + 1 ;
+
+   while (sorteio.includes(numero)) {
+    numero = getRandomInt(60) + 1 ;
 }
-for(let i = 0 ; i < 6 ; i++){
-    console.log(getRandomInt(60) + 1 );
-}
+sorteio.push(numero); 
+ }
 
 
+console.log("Números sorteados:", sorteio);
+
+// while 
 // guardar variavies array 
 
 // comparar com o primeiro numero 
 
 // dentro do if fazer um while 
-
